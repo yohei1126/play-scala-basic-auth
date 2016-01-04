00c120b2bbd91e53c4d7a678e5771fdc3cfa089f
Yohei Onishi <vivre214@gmail.com>
2016-01-04 17:00:50 +0900

    add http request

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,29 @@
 package controllers
 
-import play.api._
+import play.api.libs.ws.WSAuthScheme.BASIC
+import play.api.libs.ws.WS
 import play.api.mvc._
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 class Application extends Controller with AdminSecure {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def authSuccess = Action.async {
+    import play.api.Play.current
+    WS.url("http://localhost:9000/adminHome").withAuth("admin", "password", BASIC).get().map(
+      response => Ok(response.body)
+    )
+  }
+
+  def authFail = Action.async {
+    import play.api.Play.current
+    WS.url("http://localhost:9000/adminHome").withAuth("admin", "passord", BASIC).get().map(
+      response => Ok(response.body)
+    )
   }
 
   def adminHome = AdminAction { implicit request =>
